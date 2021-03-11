a95cfe5f98525c221f2f883730b18b2f5c19f7a0
pavelukr <pavlik99tishkov@gmail.com>
2021-03-11 18:43:17 +0200

    Lesson 6

--- spec/acceptance/answer/create_answer_spec.rb
+++ spec/acceptance/answer/create_answer_spec.rb
@@ -6,7 +6,7 @@ As an authenticated user
 ' do
 
   given(:user) { create(:user) }
-  given(:question) { create(:question, { user: user }) }
+  given!(:question) { create(:question, { user: user }) }
 
   scenario 'Authenticated user answers to the question', js: true do
     sign_in(user)
@@ -20,11 +20,9 @@ As an authenticated user
   end
 
   scenario 'Non-authenticated user answers to the question' do
-    create :question, { user: user }
     visit '/questions'
     click_on 'Show'
-    click_on 'Send'
 
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(page).to_not have_content 'Send'
   end
 end
